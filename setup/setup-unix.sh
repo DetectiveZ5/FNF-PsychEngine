566a9c44d2a5e807a5555e85a060c50f6b8eed42
DetectiveZ5 <zanderrivera599@gmail.com>
2024-07-13 11:35:05 +0800

    iOS & Android pt.1
    Making compatible for iOS and Android whilst not showing the controls for other devices and removing bugs.

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -2,16 +2,11 @@
 # SETUP FOR MAC AND LINUX SYSTEMS!!!
 #
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
-# https://haxe.org/download/version/4.2.5/
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+# https://haxe.org/download/version/4.2.5
+# Make sure hmm is installed (only needed once)
+cd ..
+haxelib --global install hmm
+haxelib --global run hmm setup
+hmm install
+haxelib run lime setup
+exit
